5fdc5aa0c8f02ed5effe23c7b11a5980f4b9e8b1
James Durbin <kdurbin@dhcp-57-241.cse.ucsc.edu>
2010-01-15 13:32:55 -0800

    Updated weka version to 1.7.0.   Added wrapper class, ExperimentSpec, to parse and hold the details of a single experiment in a bulk classifier evaluation.

---
+++ src/durbin/digma/ExperimentSpec.groovy
@@ -0,0 +1,44 @@
+package durbin.digma;
+
+import weka.attributeSelection.*
+
+
+/**
+* Class to hold a single experiment specification. 
+*/ 
+class ExperimentSpec{
+   def classifier
+   def attributeEval
+   def attributeSearch
+   def numAttributes
+   def classAttribute
+   
+   // Just to have something handy to print
+   def classifierStr
+   def attrEvalStr
+   def attrSearchStr
+   
+    //time estimate:   {so batching can divide it up reasonably}
+    
+   def ExperimentSpec(line){
+    def fields = line.split(",")
+    classifierStr = fields[0]
+    classifier = DigmaPipeline.classifierFromSpec(classifierStr)
+    
+    attrEvalStr = fields[1] 
+    attributeEval = DigmaPipeline.evalFromSpec(attrEvalStr)  
+    
+    
+    // KJD Temporary until I look up how to do this with Class.forName()
+    attrSearchStr = fields[2]
+    attributeSearch = new Ranker()
+
+    //attributeSearch = 
+    // Need to convert this to a class...
+    
+    numAttributes = fields[3] as Integer
+    classAttribute = fields[4]     
+   }    
+}
+
+//classifier, attributeEval,attributeSearch,numAttributes,classAttribute"
