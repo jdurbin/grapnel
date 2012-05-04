dcf09bed1bcbccd12e345aca7b1864100545dc3e
James Durbin <james@soe.cse.ucsc.edu>
2012-05-03 23:51:44 -0700

    Restore Multidimensional map to earlier version.

--- src/durbin/util/MultidimensionalMap.groovy
+++ src/durbin/util/MultidimensionalMap.groovy
@@ -171,95 +171,28 @@ class TwoDMap extends MultidimensionalMap{
 			return(map)
 		}
 
-
-
-		/**
+    
+    /**
     * Print out a 2D table that has max keys in each direction...
     */ 
-/*  Bizarre... this doesn't work... the mere introduction of new File()
-    breaks it...
-
-    def writeTable(String fileName,delimiter,nullVal){
+    def printTable(delimiter,nullVal){
 
       def rowKeys = rowKeySet()
-			def colKeys = colKeySet()
 
-			bob = new File(fileName)
+      // Find the union of all column keys...
+//      def colKeys = [] as Set
+ //     rowKeys.each{rowKey->
+  //      colKeys = colKeys + this[rowKey].keySet()
+   //   }
+
+			def colKeys = colKeySet()
 
       // Print the heading...
       print "Features$delimiter"
       println colKeys.join(delimiter)
 
       // Print the table proper...
-      rowKeys.each{rowKey->
-        def rowVals = []
-        colKeys.each{colKey->
-          def val = this[rowKey][colKey]
-          if (val == [:]) rowVals << nullVal
-					else if (val == "null") rowVals << nullVal
-					else if (val == "NULL") rowVals << nullVal
-          else rowVals << val
-        }
-        print  "$rowKey$delimiter"
-        println rowVals.join(delimiter)        
-      }
-    }
-*/
-		
 
-	  def printTable(delimiter,nullVal){
-			printTable(delimiter,nullVal,heading=true)
-		}
-
-
-		/**
-    * Crazy hack because can't seem to get write to work!!
-    */ 
-    def getLines(delimiter,nulVal,heading){
-
-      def rowKeys = rowKeySet()
-			def colKeys = colKeySet()
-
-			def lines = []
-
-			if (heading){
-      	// Print the heading...				
-      	def aline =  "Features$delimiter"+colKeys.join(delimiter)
-				lines << aline
-			}
-
-      // Print the table proper...
-      rowKeys.each{rowKey->
-        def rowVals = []
-        colKeys.each{colKey->
-          def val = this[rowKey][colKey]
-          if (val == [:]) rowVals << nullVal
-					else if (val == "null") rowVals << nullVal
-					else if (val == "NULL") rowVals << nullVal
-          else rowVals << val
-        }
-				def aline = "$rowKey$delimiter" + rowVals.join(delimiter) 
-				lines << aline
-      }
-			return(lines)
-    }
-
-    
-    /**
-    * Print out a 2D table that has max keys in each direction...
-    */ 
-    def printTable(delimiter,nullVal,heading){
-
-      def rowKeys = rowKeySet()
-			def colKeys = colKeySet()
-
-			if (heading){
-      	// Print the heading...
-      	print "Features$delimiter"
-      	println colKeys.join(delimiter)
-			}
-
-      // Print the table proper...
       rowKeys.each{rowKey->
         def rowVals = []
         colKeys.each{colKey->
@@ -274,6 +207,8 @@ class TwoDMap extends MultidimensionalMap{
       }
     }
     
+
+
 
 		def writeTable(File out){
 			writeTable(out,"\t","NA"){}
@@ -285,28 +220,23 @@ class TwoDMap extends MultidimensionalMap{
 		
 
 		def writeTable(String fileName){
-			writeTable(fileName,"\t","NA")
+			writeTable(fileName,"\t","NA"){}
 		}
 		
-		def writeTable(String fileName,delimiter,nullVal){
+		def writeTable(fileName,delimiter,nullVal){
 			out = new File(fileName)
-			writeTable(out,delimiter,nullValue){
-				it
-			}
+			writeTable(fileName,delimiter,nullValue){}
 		}
-		
-		
-		/**
+
+
+
+
+    /**
     * Write out a 2D table that has max keys in each direction...
     */ 
-    def writeTable(String fileName){
+    def writeTable(File out,delimiter,nullVal,Closure c){
 
-			out = new File(fileName)
-			delimiter = "\t"
-			nullVal = "null"
       out.withWriter(){w->
-	
-				err.println "DEBUG***** "+this.class
 
         def rowKeys = this.keySet()
 
@@ -314,14 +244,7 @@ class TwoDMap extends MultidimensionalMap{
         def colKeys = [] as Set
                   
         rowKeys.each{rowKey->
-					err.println "rowKey = $rowKey"
-					err.println "this.clas="+this.class
-					err.println "this.[rowkey]= "+this[rowKey]
-					//err.println "this[rowKey].class"+bob.class
-					rowKeyRow = this[rowKey]
-					err.println "rowKeyRow.class="+rowKeyRow.class
-					
-          colKeys = colKeys + (rowKeyRow.keySet())
+          colKeys = colKeys + this[rowKey].keySet()
         }
 
         // Print the heading...
@@ -335,14 +258,14 @@ class TwoDMap extends MultidimensionalMap{
           colKeys.each{colKey->
             def val = this[rowKey][colKey]
             if (val == [:]) rowVals << nullVal
-            else rowVals << val
+            else rowVals << c.call(val)
           }
           w.write("$rowKey$delimiter")
           w.writeLine(rowVals.join(delimiter))
         }
       }        
     }
-		
+
 
 
 /**   Clean up and make a merge function...
