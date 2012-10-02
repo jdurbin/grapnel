fe7dc4bc26c4c6330a33c73da70b1fcede370bf9
James <james@dhcp-63-192.cse.ucsc.edu>
2012-10-02 12:03:10 -0700

    CounterMap... common pattern.

---
+++ src/durbin/util/CounterMap.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/env groovy 
+
+
+/***
+* Eventually I'd like to do something where this syntax works whether
+* or not 'bob' has already been added to the map...
+*
+* cm = new CounterMap()
+*
+* cm['bob']++
+*
+* Not sure if that is even possible because getAt() has no way to know
+* if it's being invoked in a case where the value should be supplied or
+* where the value will be operated on by math. 
+* 
+* In the mean time, will just have the simpler to implement:
+*
+* cm.inc('bob')
+*/
+
+
+class CounterMap extends HashMap{	
+	def inc(key){
+		if (this.containsKey(key)){
+			count = this[key]
+			count++
+			this[key] = count
+		}else{
+			this[key] = 1
+		}
+	}		
+}
+
