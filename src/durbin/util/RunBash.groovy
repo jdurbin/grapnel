ced8210cff245854da0002be75182184e2eaa2cb
James <james@Jamess-MacBook-Pro.local>
2012-10-07 23:45:56 -0700

    Change RunBash to print output as it goes rather than after bash process is finished.

--- src/durbin/util/RunBash.groovy
+++ src/durbin/util/RunBash.groovy
@@ -38,7 +38,6 @@ class RunBash{
     pb.redirectErrorStream(true); // use this to capture messages sent to stderr
     Process shell = pb.start();
     InputStream shellIn = shell.getInputStream(); // this captures the output from the command
-    int shellExitStatus = shell.waitFor(); // wait for the shell to finish and get the return code
 
     // at this point you can process the output issued by the command
     // for instance, this reads the output and writes it to System.out:
@@ -46,6 +45,10 @@ class RunBash{
     while ((c = shellIn.read()) != -1){
       System.out.write(c);
     }
+
+		// this was before shellIn.read(), but that meant that didn't see shell output
+		// until it was done... it does work here, right?
+ 		int shellExitStatus = shell.waitFor(); // wait for the shell to finish and get the return code
 
     // close the stream
     try {
