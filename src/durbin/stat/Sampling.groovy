962278ce56497266135eb0805e5fa69027efb3a5
James Durbin <durbin.james@gmail.com>
2015-03-09 12:46:45 -0700

    Add method to return random selection and indices

--- src/durbin/stat/Sampling.groovy
+++ src/durbin/stat/Sampling.groovy
@@ -15,7 +15,26 @@ class Sampling{
 			new MersenneTwister((int)System.currentTimeMillis()));
 	    def outlist = []
 	    values.each{i->
-		outlist	<< list[(int)i]
+			outlist	<< list[(int)i]
 	    }
+		return(outlist);		
 	}		
+	
+	
+	/**
+	* Returns a random subset of the given list.  
+	*/ 
+	public static def randomSubsetWithIndexes(list,n){
+	    def values = new long[n]
+	    RandomSampler.sample(n,list.size(),n,0,values,0,
+			new MersenneTwister((int)System.currentTimeMillis()));
+	    def outlist = []
+		def outvalues = []
+	    values.each{i->
+			outlist	<< list[(int)i]
+			outvalues << (int)i
+	    }		
+		return([outlist,outvalues])
+	}	
+	
 }
