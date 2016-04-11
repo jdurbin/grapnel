15c65bafc2c0000cede0a37ca73ed6c769aadbac
James Durbin <durbin.james@gmail.com>
2016-04-10 22:11:20 -0700

    Make .bash() return stdout rather than simply echoing stdout to stdout

--- src/durbin/util/RunBash.groovy
+++ src/durbin/util/RunBash.groovy
@@ -15,47 +15,52 @@ class RunBash{
 	
 	static boolean bEchoCommand = false;
   
-  // Add a bash() method to GString and String 
-  static def enable(){
-    GString.metaClass.bash = {->
-      RunBash.bash(delegate)
-    }    
-    String.metaClass.bash = {->
-        RunBash.bash(delegate)
-    }    
-  }
+	// Add a bash() method to GString and String 
+	static def enable(){
+		GString.metaClass.bash = {->
+			RunBash.bash(delegate)
+		}    
+		String.metaClass.bash = {->
+			RunBash.bash(delegate)
+		}    
+	}
   
 	static def bash(cmd){
 
-    cmd = cmd as String
+		StringBuffer stdbuf = new StringBuffer()
+
+		cmd = cmd as String
 
 		if (bEchoCommand) {
 			System.err.println cmd.replaceAll("\t","")
 		}
 
-    // create a process for the shell
-    ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
-    pb.redirectErrorStream(true); // use this to capture messages sent to stderr
-    Process shell = pb.start();
+		// create a process for the shell
+		ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
+		pb.redirectErrorStream(true); // use this to capture messages sent to stderr
+		Process shell = pb.start();
 		shell.getOutputStream().close();
-    InputStream shellIn = shell.getInputStream(); // this captures the output from the command
+		InputStream shellIn = shell.getInputStream(); // this captures the output from the command
 
-    // at this point you can process the output issued by the command
-    // for instance, this reads the output and writes it to System.out:
-    int c;
-    while ((c = shellIn.read()) != -1){
-      System.out.write(c);
-    }
+		// at this point you can process the output issued by the command
+		// for instance, this reads the output and writes it to System.out:
+		char c;
+		while ((c = shellIn.read()) != -1){
+			//System.out.write(c);
+			stdbuf.append(c)
+		}
 
 		// this was before shellIn.read(), but that meant that didn't see shell output
 		// until it was done... it does work here, right?
- 		int shellExitStatus = shell.waitFor(); // wait for the shell to finish and get the return code
+		int shellExitStatus = shell.waitFor(); // wait for the shell to finish and get the return code
 
-    // close the stream
-    try {
+		// close the stream
+		try {
 			shellIn.close();
 			pb = null;
 			shell = null;
 		} catch (IOException ignoreMe) {}
-  }
+		return(stdbuf)	
+	}
 }
+		
