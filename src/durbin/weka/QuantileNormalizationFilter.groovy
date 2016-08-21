04174c90b168054a7ee379840fb5a0bd77e9238b
James Durbin <durbin.james@gmail.com>
2016-08-20 21:30:24 -0700

    Add icon to viewtab when running on Mac.

--- src/durbin/weka/QuantileNormalizationFilter.groovy
+++ src/durbin/weka/QuantileNormalizationFilter.groovy
@@ -6,6 +6,7 @@ import weka.core.Capabilities.Capability;
 import weka.filters.*;
 
 import durbin.stat.QuantileNormalization;
+import hep.aida.bin.QuantileBin1D;
 
 /****
 * Wraps QuantileNormalization in boilerplate to be used as a Weka filter. 
@@ -15,25 +16,52 @@ public class QuantileNormalizationFilter extends SimpleBatchFilter {
 	static{WekaAdditions.enable();}
 	static err = System.err
 	
-	QuantileNormalization qn = new QuantileNormalization()
+	//QuantileNormalization qn = new QuantileNormalization()
+	def qn = new QuantileBin1D(0.002)
 	
 	def setTrainingDistribution(trainingData){
-		def allvalues = getAllValues(trainingData)
-		qn.compressDistribution(allvalues)
+		def allvalues = getAllValues(trainingData)	
+		def count = 0;	
+		allvalues.each{
+			try{
+				qn.add(it)
+			}catch(Exception e){
+				println "Exception at it = $it \t count=$count"
+			}
+			count++
+		}
+		//qn.compressDistribution(allvalues)
 	}
 	
 	def setNumQuantiles(numQuantiles){
-		qn.setNumQuantiles(numQuantiles)
+		//qn.setNumQuantiles(numQuantiles)
 	}
 	
 	protected Instances process(Instances instances) throws Exception {	
 		Instances result = new Instances(determineOutputFormat(instances), 0);
 
-		instances.each{instance->
-			newValues = qn.transform(instance.toDoubleArray());
+		instances.eachWithIndex{instance,i->
+			//def newValues = qn.transform(instance.toDoubleArray());
+			def newValues = transform(instance)
+			err.println "Adding ${newValues.length} values for instance $i"
 			result.add(new Instance(1, newValues));			
 		}
 		return result;
+	}
+	
+	def transform(instance){
+		def vals = instance.toDoubleArray()
+		def testQuantileBin = new QuantileBin1D(0.002) 
+		vals.each{value->
+			testQuantileBin.add(value)
+		}
+				
+		def newvals = new double[vals.length]
+		vals.eachWithIndex{v,i->
+			def testquant = testQuantileBin.quantileInverse(v)
+			newvals[i] = qn.quantile(testquant)
+		}
+		return(newvals)
 	}
 
 	/**
